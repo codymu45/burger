0a0e8f944dc9200c6b34a2ea0e0044458bca036e
Cody Mullen <mullen.cody@yahoo.com>
2020-11-01 16:46:49 -0800

    creating file structure

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_DB;
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+("Bacon Cheeseburger", 0),
+("Half Pound Steak Burger", 0),
+("Pablano Burger", 1),
+("Jack Daniels Burger", 0);
+
