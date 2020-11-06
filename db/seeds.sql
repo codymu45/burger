deb243cc942edbb6a26e2e224e6a89bbd660bd12
Cody Mullen <mullen.cody@yahoo.com>
2020-11-05 21:08:10 -0800

    truncated table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,5 @@
 USE burgers_DB;
 
 TRUNCATE burgers;
-INSERT INTO burgers (burger_name, devoured) 
-VALUES 
-("Bacon Cheeseburger", 0),
-("Half Pound Steak Burger", 0),
-("Pablano Burger", 1),
-("Jack Daniels Burger", 0);
+
 
