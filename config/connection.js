eaa08c5fe57043248b0a2438478a64ea0f8c8a98
Cody Mullen <mullen.cody@yahoo.com>
2020-11-07 09:56:43 -0800

    changed pool to connection

--- config/connection.js
+++ config/connection.js
@@ -18,27 +18,27 @@ var source = {
 
 // Creating our connection
 var connection;
-connection = mysql.createPool(source.localhost);
+connection = mysql.createConnection(source.localhost);
 
-function handleDisconnect() {
-  connection = mysql.createPool(source.localhost);
+// function handleDisconnect() {
+//   connection = mysql.createConnection(source.localhost);
                                                 
-  connection.connect(function(err) {              
-    if(err) {                                   
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);
-    }                                    
-  });                                     
+//   connection.connect(function(err) {              
+//     if(err) {                                   
+//       console.log('error when connecting to db:', err);
+//       setTimeout(handleDisconnect, 2000);
+//     }                                    
+//   });                                     
                                          
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                        
-    } else {                                      
-      throw err;                                  
-    }
-  });
-}
+//   connection.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+//       handleDisconnect();                        
+//     } else {                                      
+//       throw err;                                  
+//     }
+//   });
+// }
 
 // handleDisconnect();
 
