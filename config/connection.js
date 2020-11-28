02ba233c8aca491b0203c4a12cc5076a50387f23
Cody Mullen <mullen.cody@yahoo.com>
2020-11-27 18:42:17 -0800

    adjucted connection to jaws db

--- config/connection.js
+++ config/connection.js
@@ -3,44 +3,17 @@ var mysql = require("mysql");
 // Setting up our connection information
 var source = {
   localhost: {
-    // host: "mullenburger.herokuapp.com",
-    // port: 3306,
-    // user: "root",
-    // password: "password",
-    // database: "burgers_DB"
-    host: "us-cdbr-east-02.cleardb.com",
+    host: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "b8976df18bd51f",
-    password: "c27e1428",
-    database: "heroku_48c252aa70bd948"
+    user: "	xehbxqv494eg9glt",
+    password: "jf9gjdlbrbyl6jfe",
+    database: "m1ozsvwxk0vbtfps"
   }
 };
 
 // Creating our connection
 var connection;
 connection = mysql.createConnection(source.localhost);
-
-// function handleDisconnect() {
-//   connection = mysql.createConnection(source.localhost);
-                                                
-//   connection.connect(function(err) {              
-//     if(err) {                                   
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000);
-//     }                                    
-//   });                                     
-                                         
-//   connection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-//       handleDisconnect();                        
-//     } else {                                      
-//       throw err;                                  
-//     }
-//   });
-// }
-
-// handleDisconnect();
 
 // Connecting to the database.
 connection.connect(function(err) {
